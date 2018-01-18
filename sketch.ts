85f2cb8f6d6d2c29a3a6f4f7b470a528cfaeb771
Gareth Williams <github@randomsyntax.net>
2018-01-18 21:56:08 +0000

    -using p5 with skletch arg to make more self contained

--- sketch.ts
+++ sketch.ts
@@ -1,18 +1,23 @@
-var p = new p5();
 
-function setup() {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+
+var sketch = (p:p5) => {
+
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight);
+    }
+    
+    p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
+    
+    p.draw = () => {
+        p.background(100);
+        p.stroke(0);
+        p.strokeWeight(2);
+        p.fill(255);
+        p.rect(10,10,50,50);
+    
+    }
 }
 
-function windowResized() {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-}
-
-function draw() {
-    p.background(100);
-    p.stroke(0);
-    p.strokeWeight(2);
-    p.fill(255);
-    p.rect(10,10,50,50);
-
-}
+var p = new p5(sketch);
