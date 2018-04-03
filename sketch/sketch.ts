890f3430c696787db6356034b2caa2a67e114860
Gareth Williams <github@randomsyntax.net>
2018-04-03 16:58:42 +0100

    -Updated exampel to usee Processing Morph example

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,6 +1,6 @@
-var sketch = (p:p5) => {
+var sketch = (p: p5) => {
 
-    this.morph = new Morph();
+    const morph = new Morph();
 
     p.preload = () => {
 
@@ -8,21 +8,16 @@ var sketch = (p:p5) => {
     
     p.setup = () => {
         p.createCanvas(p.windowWidth, p.windowHeight);
-        this.morph.setup(p);
+        morph.setup(p);
     }
     
     p.windowResized = () => {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     }
     
-    //demo vars
-    let positionX = 10;
-    let positionY = 10;
-    let cubeSize = 50;
-
     p.draw = () => {
         p.background(100);
-        this.morph.draw(p);        
+        morph.draw(p);        
     }
 }
 
