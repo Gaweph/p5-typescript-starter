094add4dbcbd5060c0aab150c6a773eae1401a4d
Gareth Williams <github@randomsyntax.net>
2018-12-27 17:19:47 +0000

    -minor cleanup of files

--- sketch/morph.ts
+++ sketch/morph.ts
@@ -1,8 +1,8 @@
 //https://processing.org/examples/morph.html
 class Morph {
-    
+
   // Arrays to store the vertices for the shapes
-  shapes: {points: p5.Vector[], color: p5.Color}[];
+  shapes: { points: p5.Vector[], color: p5.Color }[];
   currentShape: number;
   // An array for the set of vertices beign drawn into the window
   morph: p5.Vector[];
@@ -14,18 +14,18 @@ class Morph {
     // Setup shapes array
     this.shapes = [];
     this.currentShape = 0;
-    this.shapes.push({points: Shapes.circle(p, 100), color: p.color('#009CDF')});
-    this.shapes.push({points: Shapes.circle(p, 150), color: p.color(255, 204, 0)});
-    this.shapes.push({points: Shapes.square(p, 50), color: p.color(175, 100, 220)});
+    this.shapes.push({ points: Shapes.circle(p, 100), color: p.color('#009CDF') });
+    this.shapes.push({ points: Shapes.circle(p, 150), color: p.color(255, 204, 0) });
+    this.shapes.push({ points: Shapes.square(p, 50), color: p.color(175, 100, 220) });
     // this.shapes.push({points: Shapes.star(p, 0, 0, 30, 70, 5), color: p.color('#E23838')});
-    
+
     // setup morph array
     this.morph = new Array<p5.Vector>();
     let highestCount = 0;
-    for(var i =0; i < this.shapes.length; i++) {
+    for (var i = 0; i < this.shapes.length; i++) {
       highestCount = Math.max(highestCount, this.shapes[i].points.length);
     }
-    for(var i =0; i < highestCount; i++) {
+    for (var i = 0; i < highestCount; i++) {
       this.morph.push(new p5.Vector());
     }
   }
@@ -33,7 +33,7 @@ class Morph {
   recalc(p: p5) {
     // We will keep how far the vertices are from their target
     var totalDistance = 0;
-    
+
     // Look at each vertex
     const points = this.shapes[this.currentShape].points;
     for (var i = 0; i < points.length; i++) {
@@ -50,33 +50,33 @@ class Morph {
     // If all the vertices are close, switch shape
     if (totalDistance < 0.1) {
       this.currentShape++;//= !this.state;
-      if(this.currentShape >= this.shapes.length) {
+      if (this.currentShape >= this.shapes.length) {
         this.currentShape = 0;
       }
     }
   }
 
   draw(p: p5) {
-    this.recalc(p);    
-      
+    this.recalc(p);
+
     const color = this.shapes[this.currentShape].color;
     const points = this.shapes[this.currentShape].points;
 
     // Draw relative to center
-    p.translate(p.width/2, p.height/2);
+    p.translate(p.width / 2, p.height / 2);
     p.strokeWeight(4);
     // Draw a polygon that makes up all the vertices
     p.beginShape();
     p.noFill();
-    
+
     p.stroke(color);
-    for(var i = 0 ; i < points.length; i++ ){
-        var v = this.morph[i];
-        p.vertex(v.x, v.y);
+    for (var i = 0; i < points.length; i++) {
+      var v = this.morph[i];
+      p.vertex(v.x, v.y);
     }
-    
-    p.endShape(p.CLOSE);  
-    
+
+    p.endShape(p.CLOSE);
+
   }
-  
+
 }
