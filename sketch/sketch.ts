a9ed5df1b9b2865f7c21edd9f2af5c1f2779d614
Jules Fouchy <jules.fouchy@ntymail.com>
2020-10-06 19:48:42 +0200

    Remove ; in ts

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,11 +1,11 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
 }
 
 function draw() {
-
+  
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight)
 }
