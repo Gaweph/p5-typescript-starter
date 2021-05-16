68650e09a2023bed291f3374262e6976d835183e
Gareth Williams <github@randomsyntax.net>
2021-05-16 19:16:50 +0100

    removed unused global.d.ts decleration

--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,6 @@
 // This file will add both p5 instanced and global intellisence 
-import module = require('p5');
 import * as p5Global from 'p5/global' 
-
+import module = require('p5');
 export = module;
 export as namespace p5;
-declare global {
-    interface Window {
-        p5: typeof module,
-    }
-}
 
